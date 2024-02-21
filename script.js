e3b51eb0bd852fb4b32011641d77a0cdf260e20c
777dan <danya.nikitin7@gmail.com>
2024-02-21 08:28:18 +0200

    added words sets

--- script.js
+++ script.js
@@ -7,18 +7,19 @@ elem.width = screen.width;
 let isGameStarted = false;
 let isGameFinished = false;
 let pressProhibition = false;
+let isSetsShowed = false;
 
-const drawInfo = (color, text, width, height) => {
+const drawInfo = (color, text, top, width, height) => {
     let centerX = elem.width / 2 - width / 2;
     context.fillStyle = color;
-    context.fillRect(centerX, 195, width, height);
+    context.fillRect(centerX, top, width, height);
     context.font = "32px Arial";
     context.fillStyle = "Black";
     context.textAlign = "left";
     context.textBaseline = "top";
-    context.fillText(text, centerX + 5, 200);
+    context.fillText(text, centerX + 5, top + 5);
 }
-drawInfo('lightblue', 'Почати гру', 175, 40);
+drawInfo('lightblue', 'Почати гру', 195, 175, 40);
 
 let Word = function (text, colour = "lightblue", top, left, width, height, isCorrect = false) {
     this.text = text;
@@ -56,7 +57,7 @@ let Word = function (text, colour = "lightblue", top, left, width, height, isCor
                 elements.forEach(function (element) {
                     context.clearRect(element.left, element.top - multiplier, element.width, element.height + multiplier);
                 });
-                drawInfo('lightblue', 'Гра завершена', 235, 40);
+                drawInfo('lightblue', 'Гра завершена', 195, 235, 40);
             }
         }, 30);
     };
@@ -71,13 +72,37 @@ const drawUntransWord = (word) => {
     context.fillText(word, 720, 15);
 }
 
-const translatedWords = [
-    [[new Word("apple", undefined, 100, 690, 60, 15, true), new Word("house", undefined, 100, 790, 60, 15)], "яблуко"],
-    [[new Word("garage", undefined, 100, 690, 60, 15), new Word("data", undefined, 100, 790, 60, 15, true)], "дані"],
-    [[new Word("milk", undefined, 100, 690, 60, 15), new Word("forest", undefined, 100, 790, 60, 15, true)], "ліс"],
-    [[new Word("window", undefined, 100, 690, 60, 15, true), new Word("watermelon", undefined, 100, 790, 80, 15)], "вікно"],
-    [[new Word("board", undefined, 100, 690, 60, 15, true), new Word("door", undefined, 100, 790, 60, 15)], "дошка"]
-];
+let translatedWords = [];
+
+const wordsSets = {
+    "Їжа": [
+        [[new Word("apple", undefined, 100, 690, 60, 15, true), new Word("fruit", undefined, 100, 790, 60, 15)], "яблуко"],
+        [[new Word("pear", undefined, 100, 690, 60, 15), new Word("cucumber", undefined, 100, 790, 60, 15, true)], "огірок"],
+        [[new Word("apple", undefined, 100, 690, 60, 15), new Word("pear", undefined, 100, 790, 60, 15, true)], "груша"],
+        [[new Word("cherries", undefined, 100, 690, 60, 15, true), new Word("watermelon", undefined, 100, 790, 80, 15)], "вишня"],
+        [[new Word("tomato", undefined, 100, 690, 60, 15), new Word("orange", undefined, 100, 790, 60, 15, true)], "апельсин"]
+    ],
+    "Природа": [
+        [[new Word("frog", undefined, 100, 690, 60, 15), new Word("forest", undefined, 100, 790, 60, 15, true)], "ліс"],
+        [[new Word("lake", undefined, 100, 690, 60, 15, true), new Word("lace", undefined, 100, 790, 60, 15)], "озеро"],
+        [[new Word("rat", undefined, 100, 690, 60, 15), new Word("river", undefined, 100, 790, 60, 15, true)], "річка"],
+        [[new Word("mountain", undefined, 100, 690, 60, 15, true), new Word("watermelon", undefined, 100, 790, 80, 15)], "гора"],
+        [[new Word("tomato", undefined, 100, 690, 60, 15), new Word("field", undefined, 100, 790, 60, 15), true], "поле"]
+    ],
+    "IT": [
+        [[new Word("web itelligence", undefined, 100, 690, 60, 15), new Word("web development", undefined, 100, 790, 60, 15, true)], "веб-розробка"],
+        [[new Word("artificial intelligence", undefined, 100, 690, 60, 15, true), new Word("artist", undefined, 100, 790, 60, 15)], "штучний інтелект"],
+        [[new Word("security key", undefined, 100, 690, 60, 15), new Word("network security", undefined, 100, 790, 60, 15, true)], "мережева безпека"],
+        [[new Word("cybertrack", undefined, 100, 690, 60, 15), new Word("cybersecurity", undefined, 100, 790, 80, 15, true)], "кібербезпека"],
+        [[new Word("software", undefined, 100, 690, 60, 15, true), new Word("sofa", undefined, 100, 790, 60, 15)], "програмне забезпечення"]
+    ]
+}
+
+const setsData = {
+    "Їжа": [420.5, 195],
+    "Природа": [420.5, 245],
+    "IT": [420.5, 295]
+};
 
 let counter = 0;
 let corAnsws = 0;
@@ -94,7 +119,18 @@ let drawScore = function () {
 };
 
 const pushWords = (counter) => translatedWords[counter][0].map((word) => elements.push(word));
-pushWords(counter);
+
+const drawSets = () => {
+    context.clearRect(680.5, 195, 175, 40);
+    context.font = "15px Arial";
+    context.fillStyle = "Black";
+    context.fillText(`Виберіть набір слів`, 680, 195);
+    let topOffset = 0;
+    for (const key in wordsSets) {
+        drawInfo('lightblue', key, 195 + topOffset, 175, 40);
+        topOffset += 50;
+    }
+}
 
 
 elem.addEventListener('click', function (event) {
@@ -102,8 +138,23 @@ elem.addEventListener('click', function (event) {
     let x = event.pageX - elemLeft,
         y = event.pageY - elemTop;
     if (!isGameStarted) {
-        if (y > 195 && y < 235 && x > 420.5 && x < 870) {
-            context.clearRect(680.5, 195, 175, 40);
+        if (!isSetsShowed) {
+            if (y > 195 && y < 235 && x > 420.5 && x < 870) {
+                drawSets();
+                isSetsShowed = true;
+            }
+        } else {
+            for (const key in setsData) {
+                if (y > setsData[key][1] && y < setsData[key][1] + 50 && x > 420.5 && x < 870) {
+                    translatedWords = wordsSets[key];
+                    break;
+                }
+            }
+            pushWords(counter);
+            for (const key in setsData) {
+                context.clearRect(680.5, setsData[key][1], 175, 40);
+            }
+            // context.clearRect(680.5, 195, 175, 40);
             isGameStarted = true;
             drawScore();
             elements.forEach(function (element) {
@@ -142,7 +193,7 @@ elem.addEventListener('click', function (event) {
                         elements.forEach(function (element) {
                             context.clearRect(element.left, element.top, element.width, element.height);
                         });
-                        drawInfo('lightblue', 'Гра завершена', 235, 40);
+                        drawInfo('lightblue', 'Гра завершена', 195, 235, 40);
                     } else {
                         drawUntransWord(translatedWords[counter][1]);
                         elements.forEach(function (element) {
